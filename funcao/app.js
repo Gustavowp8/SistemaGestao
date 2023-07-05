f1ce52d28b51c967fbd79ef4f2bb7b0c87bfe688
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2023-07-05 17:32:13 -0300

    Add files via upload
    Novos arquivos de tela

--- funcao/app.js
+++ funcao/app.js
@@ -1,11 +1,24 @@
+
+
 function entra(){
     alert("Fucniona")
 }
 
 function entra(){
     telaUm.style.display='flex'
-  }
+}
 
-  function fFechaTela(){
+function fFechaTela(){
     telaUm.style.display='none'
-  }
+}
+
+
+/***** menu ******/
+
+const btnMobile = document.getElementById('btn-mobile');
+btnMobile.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
